b7ad77eb3e0997949a7984f195effd5180fb7c23
ravi bhushan <rvbhushan18@gmail.com>
2016-08-15 01:07:01 +0530

    Add files via upload

---
+++ autoresponseform.bat
@@ -0,0 +1,6 @@
+:m
+start "" "C:\Program Files\Mozilla Firefox\firefox.exe" -new-tab "https://docs.google.com/forms/d/1P4PyUkUWbPbDdQzKOov7zY6DVLLCGrjQJBKBJioXPGw/formResponse?ifq&entry.818255482=wo&entry_277259296=10101010&entry_35008865=S.P.+Gupta&entry_480494384=yeyarahaihaiiiiiiii&submit=Submit"
+timeout /t 7
+taskkill /IM firefox.exe
+timeout /t 7
+goto :m
